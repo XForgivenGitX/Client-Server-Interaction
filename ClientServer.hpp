a6ed135b7560adf87dd470b22328da8ae554860c
XForgivenGitX <ilkubarev1805@gmail.com>
2023-02-27 16:27:09 +0300

    Includes

---
+++ ClientServer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <memory>
+
+#define BOOST_ASIO_DISABLE_HANDLER_TYPE_REQUIREMENTS
+
+#include <boost/core/noncopyable.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/thread/thread.hpp>
+
+#define io__ boost::asio
+
+#include "ConnectionData.hpp"
+#include "TaskProcessor.hpp"
+#include "TaskWrapped.hpp"
