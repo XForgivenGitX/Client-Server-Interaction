7afc25454823442c0ed757886e55e80ed64d20d7
XForgivenGitX <ilkubarev1805@gmail.com>
2023-04-05 15:59:57 +0300

    added scripts for linux

--- client/client_session/startup_client.cpp
+++ client/client_session/startup_client.cpp
@@ -2,10 +2,9 @@
 
 int main(int argc, char **argv)
 {
-    system("chcp 1251");
     lg::init_logger();
     io__::io_context ios;
-    client::client_session client(ios, {9001, argv[1]});
+    client::client_session client(ios, {9001, "127.0.0.1"});
     auto lamd = [&ios]{ios.run();};
     std::thread th1(lamd), th2(lamd);
     th1.join();
--- server/acceptor/startup_server.cpp
+++ server/acceptor/startup_server.cpp
@@ -1,11 +1,11 @@
 #include "acceptor.hpp"
 #include "connections_manager.hpp"
+
 using namespace server;
 
 int main(int argc, char** argv) 
 { 
     lg::init_logger();
-    system("chcp 1251");
     // if(argc != 2) 
     // {
     //     std::cerr << "Incorrect arguments. Usage: <port> \n";
